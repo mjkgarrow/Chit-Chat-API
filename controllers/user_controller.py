b0ab8a751ffbe2f3684f583e09fd6316c3c6e339
Matt Garrow <mjkgarrow@gmail.com>
2023-02-28 14:38:05 +1100

    created route to get all users

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -2,7 +2,7 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask import Blueprint, abort, request, jsonify
 from main import db
 from models.users import User
-# from schemas.user_schema import user_schema, users_schema
+from schemas.user_schema import user_schema, users_schema
 
 
 users = Blueprint("users", __name__, url_prefix="/users")
@@ -10,6 +10,5 @@ users = Blueprint("users", __name__, url_prefix="/users")
 
 @users.get("/")
 def get_users():
-    # user_list =
-    # return jsonify(users_schema.dump(users_list))
-    return jsonify(message="list of all users")
+    users_list = db.session.execute(db.select(User)).scalars()
+    return jsonify(users_schema.dump(users_list))
