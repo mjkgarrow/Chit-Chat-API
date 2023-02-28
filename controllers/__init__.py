8060e2a9239c6ec46bb36adc0b42ac1986511ce7
Matt Garrow <mjkgarrow@gmail.com>
2023-02-28 14:56:08 +1100

    added index controller

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,9 +1,11 @@
+from controllers.index_controller import index
 from controllers.user_controller import users
 from controllers.reset_all import reset
 # from controllers.chat_controller import chats
 
 
 registerable_controllers = [
+    index,
     users,
     reset
 ]
