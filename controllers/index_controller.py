08bf7534a5553453b4ab9622d75dbffb2b1fb48e
Matt Garrow <mjkgarrow@gmail.com>
2023-02-28 14:55:41 +1100

    added index route

---
+++ controllers/index_controller.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, jsonify
+
+
+index = Blueprint("index", __name__)
+
+
+@index.get("/")
+def get_index():
+    return jsonify(message="Welcome to Chit-Chat, a RESTful API for chatting with friends")
