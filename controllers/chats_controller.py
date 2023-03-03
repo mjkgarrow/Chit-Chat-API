c76e933309b5d1c79dce6ad30598c65cbcf67f3e
Matt Garrow <mjkgarrow@gmail.com>
2023-03-03 12:01:32 +1100

    added ability to join a chat

--- controllers/chats_controller.py
+++ controllers/chats_controller.py
@@ -70,7 +70,7 @@ def update_chat(chat_id):
 
     # If user or chat not in db, return error
     if not user or not chat:
-        return abort(401, description="Invalid user or chat doesn't exist")
+        return abort(401, description="Invalid user or chat")
 
     # Check if user is a member of chat
     chat_member = db.session.execute(
@@ -105,7 +105,7 @@ def delete_chat(chat_id):
 
     # If user or chat not in db, return error
     if not user or not chat:
-        return abort(401, description="Invalid user or chat doesn't exist")
+        return abort(401, description="Invalid user or chat")
 
     # Check if user is a member of chat
     chat_member = db.session.execute(
@@ -116,6 +116,46 @@ def delete_chat(chat_id):
 
     # Add chat to db
     db.session.delete(chat)
+    db.session.commit()
+
+    return jsonify(chat_schema.dump(chat))
+
+
+@chats.post("/<int:chat_id>")
+@jwt_required()
+def join_chat(chat_id):
+    """UPDATES A CHAT NAME"""
+
+    # Find verified user in db
+    user = db.session.get(User, get_jwt_identity())
+
+    # Find chat in db
+    chat = db.session.get(Chat, chat_id)
+
+    # If user or chat not in db, return error
+    if not user or not chat:
+        return abort(401, description="Invalid user or chat")
+
+    # Find member in database
+    chat_member = db.session.execute(
+        db.select(Member).filter_by(chat_id=chat.id, user_id=user.id)).scalar()
+
+    # If user is a chat member already, then abort
+    if chat_member:
+        return abort(401, description="Invalid request")
+
+    # Load user submitted chat secret
+    chat_data = chat_schema.load(request.json)
+
+    # Check if user submitted chat secret matches the requested chat secret
+    if chat.chat_passkey != chat_data["chat_passkey"]:
+        return abort(401, description="Invalid user or passkey")
+
+    member = Member(chat=chat,
+                    member=user)
+
+    # Add chat to db
+    db.session.add(member)
     db.session.commit()
 
     return jsonify(chat_schema.dump(chat))
