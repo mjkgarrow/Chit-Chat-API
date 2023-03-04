6edc60d82ac2605739efb553306b66a9ae48effd
Matt Garrow <mjkgarrow@gmail.com>
2023-03-04 14:03:54 +1100

    removed route to reset db

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -2,13 +2,11 @@ from controllers.index_controller import index
 from controllers.chats_controller import chats
 from controllers.auth_controller import auth
 from controllers.user_controller import users
-from controllers.reset_all import reset
 
 
 registerable_controllers = [
     index,
     chats,
     auth,
-    users,
-    reset
+    users
 ]
