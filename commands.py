cc8f1b4bd6eb38b5a9489ee85b7dd854c7bdf6cc
Matt Garrow <mjkgarrow@gmail.com>
2023-03-04 15:09:28 +1100

    removed duplicate CLI commands

--- commands.py
+++ commands.py
@@ -10,44 +10,6 @@ from models.members import member_association
 db_commands = Blueprint("db", __name__)
 
 
-# @db_commands.cli.command("create")
-# def create_db():
-#     db.create_all()
-#     print("Tables created")
-
-
-# @db_commands.cli.command("seed")
-# def seed_db():
-#     user1 = User(username="Matt",
-#                  password=bcrypt.generate_password_hash("1234").decode('utf-8'))
-
-#     user2 = User(username="Beth",
-#                  password=bcrypt.generate_password_hash("5678").decode('utf-8'))
-
-#     db.session.add(user1)
-#     db.session.add(user2)
-#     db.session.commit()
-
-#     chat1 = Chat(chat_name="Chat1",
-#                  chat_passkey="1234")
-
-#     chat2 = Chat(chat_name="Chat2",
-#                  chat_passkey="1234")
-
-#     user1.chats.append(chat1)
-#     user1.chats.append(chat2)
-#     user2.chats.append(chat1)
-
-#     db.session.commit()
-#     print("Table seeded")
-
-
-# @db_commands.cli.command("drop")
-# def drop_db():
-#     db.drop_all()
-#     print("Tables dropped")
-
-
 @db_commands.cli.command("reset")
 def reset_db():
     """DROP, CREATE AND SEED TABLES"""
