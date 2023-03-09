4bad603eb5544edd0524e8cc9f259ef91606e15b
Matt Garrow <mjkgarrow@gmail.com>
2023-03-09 16:47:16 +1100

    fixed syntax

--- controllers/index_controller.py
+++ controllers/index_controller.py
@@ -1,12 +1,9 @@
 from flask import Blueprint, jsonify
-from main import db, bcrypt
-from models.chats import Chat
-from models.users import User
-from models.messages import Message
 
 index = Blueprint("index", __name__)
 
 
 @index.get("/")
 def get_index():
-    return jsonify(message="Welcome to Chit-Chat, a RESTful API for chatting with friends and creating chatrooms")
+    return jsonify(message="Welcome to Chit-Chat, a RESTful API \
+        for chatting with friends and creating chatrooms")
