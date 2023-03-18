315716701c31a03c1ba329b298d36eb85a01c52f
Matt Garrow <mjkgarrow@gmail.com>
2023-03-18 14:13:56 +1100

    added repo to index endpoint output

--- controllers/index_controller.py
+++ controllers/index_controller.py
@@ -6,4 +6,4 @@ index = Blueprint("index", __name__)
 @index.get("/")
 def get_index():
     return jsonify(message="Welcome to Chit-Chat, a RESTful API \
-        for chatting with friends and creating chatrooms")
+for chatting with friends, visit https://github.com/mjkgarrow/Chit-Chat-API for endpoint documentation")
